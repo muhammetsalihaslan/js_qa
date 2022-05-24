ae879926bda27509423704fb0019b67869e4890b
salih <muhammetsalihaslan@gmail.com>
2022-05-24 17:28:18 +0300

    processing

---
+++ js_qa3.js
@@ -0,0 +1,19 @@
+/*Girilen farklı 3 tamsayının toplamını, çarpımını, en küçüğünü ve en
+büyüğünü yazdıran programı yazınız.*/
+
+//!Solution 1
+
+const number1 = +prompt('Please Enter a number1');
+const number2 = +prompt('Please Enter a number2');
+const number3 = +prompt('Please Enter a number3');
+const islem = prompt('Please Enter a operation');
+let netice;
+    if (number1 > number2 && number3, islem == "+") {
+        netice = (`number1 is greater than number2 and number3 and sum of given number is:${number1}+${number2}+${number3} = ${number1 + number2 + number3}`);
+    } else if(number1 > number2 && number3, islem == "-") {
+        netice = (`number1 is greater than number2 and number3 and subtract of given number is:${number1}+${number2}+${number3} = ${number1 - number2 - number3}`);
+    } else if(number1 > number2 && number3, islem == "*") {
+        netice = (`number1 is greater than number2 and number3 and subtract of given number is:${number1}+${number2}+${number3} = ${number1 - number2 - number3}`);
+    } 
+console.log(netice);
+
