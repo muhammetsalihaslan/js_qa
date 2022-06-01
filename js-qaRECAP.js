58dfe09ea92ce6561b586b5f8d1309b72f560fc1
salih <muhammetsalihaslan@gmail.com>
2022-06-01 19:59:40 +0300

    1

---
+++ js-qaRECAP.js
@@ -0,0 +1,11 @@
+//? 1- Write a JS code to print numbers from 1 to 10
+function printNumber(){
+    for(let i = 1; i <= 10; i++) {
+        console.log(i);
+    }
+}
+
+console.log(printNumber());
+
+//? 2-Write a JS code to print a 
+
