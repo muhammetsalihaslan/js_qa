76f38354b091467407c91e0b3603b190a5c7f11b
salih <muhammetsalihaslan@gmail.com>
2022-06-02 14:12:07 +0300

    1

---
+++ js_qa12.js
@@ -0,0 +1,11 @@
+//** Bir dairenin alanını hesaplayan fonksiyonu arrow fonksiyon olarak yazınız. Yarıçap prompt ile girilmeli ve sonuç ana programda yazdırılmalıdır.
+
+const r = parseInt(prompt("please enter r"))
+
+const alanDaire = (r) => Math.PI*r*r;
+console.log(alanDaire(`${r}`));
+
+
+
+
+
