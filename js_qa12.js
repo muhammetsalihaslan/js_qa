972547b8363469d284374885df94bccf59983f56
salih <muhammetsalihaslan@gmail.com>
2022-06-02 15:24:23 +0300

    2

--- js_qa12.js
+++ js_qa12.js
@@ -1,4 +1,4 @@
-//** Bir dairenin alanını hesaplayan fonksiyonu arrow fonksiyon olarak yazınız. Yarıçap prompt ile girilmeli ve sonuç ana programda yazdırılmalıdır.
+//? Bir dairenin alanını hesaplayan fonksiyonu arrow fonksiyon olarak yazınız. Yarıçap prompt ile girilmeli ve sonuç ana programda yazdırılmalıdır.
 
 const r = parseInt(prompt("please enter r"))
 
@@ -6,6 +6,13 @@ const alanDaire = (r) => Math.PI*r*r;
 console.log(alanDaire(`${r}`));
 
 
+//? Doğum tarihini parametre olarak alan ve ana programa yaşı hesaplayıp döndüren fonksiyonu yazınız
+
+const dogumTarihi = parseInt(prompt("please enter birth date"))
+
+const yas = (dogumTarihi) => new Date().getFullYear() - dogumTarihi
+,
+console.log(yas(`${dogumTarihi}`));//*yas(`${dogumTarihi}`) şelinde de yazılabilir
 
 
 
