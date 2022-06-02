a418837ca95fa978e52bee358dd8e5251f1175d0
salih <muhammetsalihaslan@gmail.com>
2022-06-02 15:48:09 +0300

    jaqa13-1

---
+++ js_qa13.js
@@ -0,0 +1,26 @@
+//?Taban ve yükseklik değerlerini parametre olarak alan ve bir üçgenin alanını hesaplayarak ana programa döndüren fonksiyonu yazınız.
+
+const taban = parseInt(prompt("enter a base"));
+const yukseklik = parseInt(prompt("enter a height"));
+
+function alan(taban, yukseklik) {
+    const olc = (taban*yukseklik)/2
+    return olc 
+}
+
+alan(`${taban}`,`${yukseklik}`)
+
+//! second Solution with function expression
+
+const taban = parseInt(prompt("enter a base"));
+const yukseklik = parseInt(prompt("enter a height"));
+
+const alan1 = function(taban, yukseklik) {
+    return taban*yukseklik/2
+} 
+
+alan1(`${taban}`, `${yukseklik}`)
+
+//! thirth Solution with Arrow function;
+
+
