67341c86482668ef90f915b255c3c3171d090ba4
salih <muhammetsalihaslan@gmail.com>
2022-06-20 10:17:47 +0300

    up

---
+++ js_qa23.js
@@ -0,0 +1,9 @@
+//?Write a JavaScript exercise to create a variable using a user-defined name.
+
+let name1 = 'variable';
+
+let n = 120;
+
+this[name1] = n;  //! we create a variable with user defined name this keyword is in important here
+
+console.log(name1);  
