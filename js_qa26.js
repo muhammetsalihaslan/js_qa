93f1fbf023b596378e254073bfb065a1e4243c02
salih <muhammetsalihaslan@gmail.com>
2022-06-21 16:11:49 +0300

    up1

---
+++ js_qa26.js
@@ -0,0 +1,13 @@
+const number1 = parseInt(prompt("give us a number for the number1"));
+const number2 = parseInt(prompt("give us a number for the number2"));
+let result = 0;
+if (number1 === number2) {
+    result = (number1 + number2)*3
+    console.log(result);
+} else{
+    result = (number1 + number2)
+    console.log(result);
+};
+
+
+
