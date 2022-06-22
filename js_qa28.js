8522e2e3ee2466cac5d6e64d1d66218754b691e9
salih <muhammetsalihaslan@gmail.com>
2022-06-22 15:53:54 +0300

    up2

--- js_qa28.js
+++ js_qa28.js
@@ -10,3 +10,15 @@ if (string.startsWith("py")) {
 };
 
 
+//! SOLUTİON2
+
+const string = prompt("enter something");
+
+if (string.startsWith("Py")) {
+    console.log(string);
+} else{
+    let result = string.split("");
+    result.unshift("Py");
+    result.join("");
+    console.log(result);
+};  //! it gives us an error in join("") part but its description like this first of all i use split function with ("") to convert string to array after that use unshift to add py inside to array but this add will be firt element of array and than use join("") function to convert array to string and finish 
